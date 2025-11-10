5ab8586d9988c7584fb8ef4368a51d1f66a4f52e
Harii03 <86625634+Harii03@users.noreply.github.com>
2025-11-09 23:51:13 -0500

    Refactor ECR repositories and update outputs

--- main.tf
+++ main.tf
@@ -11,16 +11,29 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# ECR Repository - this usually works even with restricted permissions
-resource "aws_ecr_repository" "clo835_repo" {
-  name = "clo835-assignment1-app"
+# ECR Repository for WebApp
+resource "aws_ecr_repository" "webapp_repo" {
+  name = "clo835-webapp"
 
   image_scanning_configuration {
     scan_on_push = true
   }
 
   tags = {
-    Name = "clo835-ecr"
+    Name = "clo835-webapp"
+  }
+}
+
+# ECR Repository for MySQL (if you need custom MySQL image)
+resource "aws_ecr_repository" "mysql_repo" {
+  name = "clo835-mysql"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "clo835-mysql"
   }
 }
 
@@ -28,7 +41,6 @@ resource "aws_security_group" "clo_sg" {
   name        = "clo-835-sg"
   description = "Security group for CLO-835 instance"
 
-  # SSH access
   ingress {
     from_port   = 22
     to_port     = 22
@@ -36,7 +48,6 @@ resource "aws_security_group" "clo_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Application ports
   ingress {
     from_port   = 8081
     to_port     = 8083
@@ -57,12 +68,11 @@ resource "aws_security_group" "clo_sg" {
 }
 
 resource "aws_instance" "clo_835" {
-  ami                    = "ami-0157af9aea2eef346"  # Amazon Linux 2023
+  ami                    = "ami-0157af9aea2eef346"
   instance_type          = "t2.micro"
   key_name               = "Assignment - 1"
   vpc_security_group_ids = [aws_security_group.clo_sg.id]
 
-  # User data to install Docker and git on startup
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
@@ -79,10 +89,12 @@ resource "aws_instance" "clo_835" {
 
 output "instance_public_ip" {
   value = aws_instance.clo_835.public_ip
-  description = "Public IP address of the EC2 instance"
 }
 
-output "ecr_repository_url" {
-  value = aws_ecr_repository.clo835_repo.repository_url
-  description = "ECR repository URL for Docker images"
+output "webapp_ecr_url" {
+  value = aws_ecr_repository.webapp_repo.repository_url
+}
+
+output "mysql_ecr_url" {
+  value = aws_ecr_repository.mysql_repo.repository_url
 }
